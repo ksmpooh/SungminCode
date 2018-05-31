b25d31b4af640e2b36bb80cf990d1189b8c80591
SungMin Kim <daverpooh@naver.com>
2018-05-31 20:50:22 +0900

    sm

--- HGU/BioData/Visualization/1.heatmap3_for_ppeng.R
+++ HGU/BioData/Visualization/1.heatmap3_for_ppeng.R
@@ -23,22 +23,26 @@ colnames(myCols)[1] <- "Result"
 colnames(myCols)[2] <- "CancerCode"
 model<-subset(model,select = -c(X,GSM_ID,cancer_code,real_y))
 #model<-subset(model,select = -c(X,GSM_ID))
-
+#model<-model[1:10,1:10]
+#model<-round(model,digits = 4)
 #model<-data.matrix(subset(model,select = -X))
 #input<-data.matrix(subset(model,select = -GSM_ID))
 input<-data.matrix(model)
 #myclust=function(c) {hclust(c,method="average")}
-
+#input <- round(input, digits = 3)
 #x = legend("topright",legend = c("Cancer","Normal"),fill = c("red","green"))
-png("heatmap.png",width = 1000, height = 1000)
-?png
+png("pre_3.png",width = 2000, height = 2000)
+#heatmep3 model_3, w = 1000, height = 100
+#??heatmap3
 heatmap3(t(input),col = bluered(100),cexRow = 0.5, cexCol = 0.5,
-         
-         margins = c(2,16),Colv = NA, #breaks,
-         breaks = seq(-1,1,length.out= 101),
-         ColSideColors = myCols,
+         main = "ppeng_3",
+         margins = c(2,16),#Colv = NA, #breaks,
+         #breaks = seq(-1,1,length.out= 1001),
+         #ColSideColors = myCols,
          scale = "row"
 ) 
+
+heatmap(t(input),col = bluered(111111))
 #heatmap3
 legend("topright",legend = c("Cancer","Normal"),fill = c("red","green")
        ,border = FALSE,bty = "n", y.intersp =2.0,cex = 2.0)
@@ -46,7 +50,10 @@ legend("topright",legend = c("Cancer","Normal"),fill = c("red","green")
 dev.off()
 
 graphics.off()
-#par("mar") 
+ #par("mar") 
 #par(mar=c(1,1,1,1))
 #intersect(colnames(model_0),colnames(model_1))
 #colnames(model_1)
+summary(model_3)
+min(model)
+a<-input[1:10,1:10]
