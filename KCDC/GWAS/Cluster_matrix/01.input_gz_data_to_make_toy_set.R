c53669c2302acd90a2f1a7c5c0e8feb38869592a
sm_samsungLabtop <daverpooh@naver.com>
2019-08-01 17:50:09 +0900

    toy set

--- KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
+++ KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
@@ -3,23 +3,25 @@ getwd()
 
 file_list <- read.table("list_window.txt")
 file_list <-unlist(file_list)
-  
 
+##############read file
 for (f in file_list[1:2]){
+  toy <- data.frame()
   #assign(paste0("model_",f), read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
   #assign(f, read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
-  assign(toy, read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
-  assign(toy,toy[sample(nrow(toy),10000),])
-  write.csv(toy,paste0("Toyset/Toy_",f,".csv"),col.names = TRUE,row.names = FALSE,quote = FALSE)
+  #assign(toy, read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
+  toy <- read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T )
+  #assign(toy,toy[sample(nrow(toy),10000),])
+  toy <- toy[sample(nrow(toy),10000),]
+  write.table(toy,paste0("Toyset/Toy_",f,".txt"),col.names = TRUE,row.names = FALSE,quote = FALSE,sep = "\t")
+  on.exit(close(toy))
 }
 
-Toy_AS_V1_B1_2nd <- AS_V1_B1_2nd[sample(nrow(AS_V1_B1_2nd),10000),]
-Toy_AS_V1_B2_2nd <- AS_V1_B2_2nd[sample(nrow(AS_V1_B2_2nd),10000),]
 
-#########################
-zz = gzfile('AS_V1_B1_2nd/Ps.performance.uniquevar.txt.gz',"rt")
-a <- read.table(zz,header = T)
-close()
-head(a)
-??close()
+for (f in file_list[1:2]){
+  assign(paste0(f),read.table(paste0("Toyset/Toy_",f,".txt"),header = T,sep = "\t"))
+}
 
+length(intersect(AS_V1_B1_2nd$probeset_id,AS_V1_B2_2nd$probeset_id))
+a <- subset(AS_V1_B1_2nd,select = c(probeset_id,ConversionType))
+colnames(a)
