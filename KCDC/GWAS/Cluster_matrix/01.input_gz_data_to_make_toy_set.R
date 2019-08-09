12c1f39a60671692ccfb40ecfe9b168a0066aae6
sm_samsungLabtop <daverpooh@naver.com>
2019-08-09 17:50:49 +0900

    data pasing

--- KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
+++ KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
@@ -5,26 +5,43 @@ file_list <- read.table("list_window.txt")
 file_list <-unlist(file_list)
 
 ##############read file
+
+
 for (f in file_list){
-  assign(paste0("model_",f), read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
-  assign(paste0("model_",f),subset(paste0("model_",f),select = c("probeset_id","ConversionType","H.W.p.Value","MinorAlleleFrequency")))
-  on.exit(close(paste0("model_",f)))
+  toy<-data.frame()
+  toy <- read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T )
+  #assign(paste0("model_",f),subset(paste0("model_",f),select = c("probeset_id","ConversionType","H.W.p.Value","MinorAlleleFrequency")))
+  #assign(paste0("model_",f),subset(paste0("model_",f),select = c("probeset_id","ConversionType")))
+  toy <- subset(toy,select = c(probeset_id,ConversionType))
+  write.table(toy,paste0("new_",f,".txt"),quote = F,row.names = F,col.names = T)
+  on.exit(close(toy))
 }
-help(subset)
 
+for (f in file_list[1]){
+  assign(paste0(f),read.table(paste0("new_set/new_",f,".txt"),header = T))
+}
 
+for (f in file_list[1]){
+  assign(paste0(f),subset(paste0(f),(ConversionType == "Other" | ConversionType == "OTV" | ConversionType =="callRateBelowThreshold")))
+}
+a<- subset(AS_V1_B1_2nd,(ConversionType == "Other" | ConversionType == "OTV" | ConversionType =="callRateBelowThreshold"))
+
+c <- merge(AS_V1_B1_2nd,AS_V1_B2_2nd,by = "probeset_id")
+length(intersect(AS_V1_B1_2nd$probeset_id,AS_V1_B2_2nd$probeset_id))
+
+colnames(model_AS_V1_B1_2nd)
 
 ##############read file
-for (f in file_list[1:4]){
+for (f in file_list[1:1]){
   toy <- data.frame()
   #assign(paste0("model_",f), read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
   
-  ##assign(toy, read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T ))
+  
   toy <- read.table(gzfile(paste0(f,"/Ps.performance.uniquevar.txt.gz"),"rt"),header = T )
   #assign(toy,toy[sample(nrow(toy),10000),])
   #toy <- toy[sample(nrow(toy),10000),]
-  toy <- toy[1:10000,]
-  write.table(toy,paste0("Toyset/Toy_",f,".txt"),col.names = TRUE,row.names = FALSE,quote = FALSE,sep = "\t")
+  #toy <- toy[1:10000,]
+  #write.table(toy,paste0("Toyset/Toy_",f,".txt"),col.names = TRUE,row.names = FALSE,quote = FALSE,sep = "\t")
   on.exit(close(toy))
   #on.exit(close(paste0("model_",f)))
 }
@@ -40,11 +57,11 @@ levels(model_DS_V1_B2_2nd$ConversionType)
 Cluster_matrix <- data.frame(row.names = rownames(model_DS_V1_B2_2nd))
 #Cluster_matrix <-cbind(Cluster_matrix,model_DS_V1_B2_2nd$ConversionType)
 
-
+#50% >
 length(which(model_DS_V1_B2_2nd$ConversionType =='Other'))
 length(which(model_DS_V1_B2_2nd$ConversionType =='OTV'))
 length(which(model_DS_V1_B2_2nd$ConversionType =='CallRateBelowThreshold'))
-length(which(model_DS_V1_B2_2nd$ConversionType =='Other' || 'CallRateBelowThreshold' || 'OTV')))
+length(which(model_DS_V1_B2_2nd$ConversionType =='Other' || 'CallRateBelowThreshold' || 'OTV'))
 
 a <- table(model_DS_V1_B2_2nd$ConversionType)
 sum(a[c('Other' , 'OTV' , 'CallRateBelowThreshold')])
@@ -57,19 +74,9 @@ v1 <- c("1","3","4")
 name<-c("kim","ha","jui")
 b<-data.frame(name,v1)
 rownames(b)<-name
-
-
-
 c <- merge(a,b,by = 0,all=T)
-#######################
-#colnames(Cluster_matrix)
-#data.frame()
-#head(Cluster_matrix)
-#AS_V1_B1_2nd$probeset_id
 
 
 
-
-
-colnames(DS_V1_B1_2nd)
-levels(model_DS_V1_B2_2nd$ConversionType)
+a<- subset(AS_V1_B1_2nd,(AS_V1_B1_2nd$ConversionType == "Other" |AS_V1_B1_2nd$ConversionType == "OTV" |AS_V1_B1_2nd$ConversionType =="callRateBelowThreshold"))
+b<=
