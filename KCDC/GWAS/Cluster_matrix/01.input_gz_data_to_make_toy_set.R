80823d3474c78bad8b739c42b7fd7bf1c2b35a15
sm_samsungLabtop <daverpooh@naver.com>
2019-08-20 08:56:38 +0900

    cluster_matrix_modi

--- KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
+++ KCDC/GWAS/Cluster_matrix/01.input_gz_data_to_make_toy_set.R
@@ -32,18 +32,39 @@ for (f in file_list){
 }
 
 
-#####################
+#####################merge 
 venn <-toy
+
+summ <- data.frame()
 for (f in file_list){
-  assign(paste0(f),read.table(paste0("only_error_set/only_error_",f,".txt"),header = T))
-  #toy <- read.table(paste0("only_error_set/only_error_",f,".txt"),header = T)
+  #assign(paste0(f),read.table(paste0("only_error_set/only_error_",f,".txt"),header = T))
+  toy <- read.table(paste0("only_error_set/only_error_",f,".txt"),header = T)
   #rownames(toy)<-toy$probeset_id
   #toy<-subset(toy,select = probeset_id)
   #assign(paste0(f),toy)
   #venn <- merge(venn,toy,all = TRUE)
+  summ <- rbind(summ,dim(toy)[1])
 }
 
-write.table(venn,"merge_error_marker_id.txt",row.names = F,col.names = T,quote = F)
+colnames(summ) <- "error_marker_count"
+rownames(summ) <- file_list
+#write.table(venn,"merge_error_marker_id.txt",row.names = F,col.names = T,quote = F)
+
 
 dim(venn)
 str(venn)
+
+
+#######################union intersect
+
+probeset_id =""
+u <- AS_V1_B1_2nd
+n <- AS_V1_B1_2nd
+for (f in file_list[2:25]){
+  #assign(paste0(f),read.table(paste0("only_error_marker_ID/only_error_maker_id_",f,".txt"),header = T))
+  toy <- read.table(paste0("only_error_marker_ID/only_error_maker_id_",f,".txt"),header = T)
+  u <- union(u,toy)
+  n <- intersect(n,toy)  
+}
+
+
--- KCDC/GWAS/Cluster_matrix/02.make_matrix.R
+++ KCDC/GWAS/Cluster_matrix/02.make_matrix.R
@@ -1,8 +1,8 @@
 
 
 library(dplyr)
-
-cluster_matrix <- read.table("C:/Users/user/Desktop/cluster_matrix/df.txt",header = T,sep = "\t",na.strings = c("","NA"))
+setwd("C:/Users/user/Desktop/cluster_matrix/")
+cluster_matrix <- read.table("df.txt",header = T,sep = "\t",na.strings = c("","NA"))
 cluster_matrix$na_count = apply(cluster_matrix, 1, function(x) sum(is.na(x)))
 numberOfbatch <- ncol(cluster_matrix)-2
 cluster_matrix$Batch_failure <- (numberOfbatch - cluster_matrix$na_count)/numberOfbatch *100
@@ -14,7 +14,20 @@ cluster_matrix$CallRateBelowThreshold <- apply(cluster_matrix,1,function(x) leng
 
 
 
+##############################probe count
+marker_list <- cluster_matrix$probeset_id
 
-#hi
-a <- 1
-a
+v1_probeset <- read.table("Axiom_KORV1.0.na34.annot.probe_count_change_v1.1ID.txt",header = T)
+
+length(intersect(v1_probeset$Probe_Set_ID,marker_list))
+#v1_probeset <- read.table("Axiom_KORV1.0.na34.annot.probe_count.txt",header= T)
+rownames(v1_probeset) <- v1_probeset$Probe_Set_ID
+v1_probeset<-v1_probeset[marker_list,]
+
+
+v2_probeset <-read.table("Axiom_KORV1.1.na35.annot.probe_count.txt",header = T)
+length(intersect(v2_probeset$Probe_Set_ID,marker_list))
+rownames(v2_probeset) <- v2_probeset$Probe_Set_ID
+v2_probeset<-v2_probeset[marker_list,]
+
+
