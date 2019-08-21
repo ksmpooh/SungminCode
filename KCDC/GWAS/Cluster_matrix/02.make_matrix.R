62142019a6b695fc57772c159003c42feb39f53f
sm_samsungLabtop <daverpooh@naver.com>
2019-08-21 15:45:23 +0900

    bmi

--- KCDC/GWAS/Cluster_matrix/02.make_matrix.R
+++ KCDC/GWAS/Cluster_matrix/02.make_matrix.R
@@ -17,16 +17,18 @@ cluster_matrix$CallRateBelowThreshold <- apply(cluster_matrix,1,function(x) leng
 ##############################probe count
 marker_list <- cluster_matrix$probeset_id
 
+#v1_probeset <- read.table("Axiom_KORV1.0.na34.annot.probe_count.txt",header= T)
 v1_probeset <- read.table("Axiom_KORV1.0.na34.annot.probe_count_change_v1.1ID.txt",header = T)
 
 length(intersect(v1_probeset$Probe_Set_ID,marker_list))
-#v1_probeset <- read.table("Axiom_KORV1.0.na34.annot.probe_count.txt",header= T)
+
 rownames(v1_probeset) <- v1_probeset$Probe_Set_ID
 v1_probeset<-v1_probeset[marker_list,]
 
 
 v2_probeset <-read.table("Axiom_KORV1.1.na35.annot.probe_count.txt",header = T)
 length(intersect(v2_probeset$Probe_Set_ID,marker_list))
+
 rownames(v2_probeset) <- v2_probeset$Probe_Set_ID
 v2_probeset<-v2_probeset[marker_list,]
 
