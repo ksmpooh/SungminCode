e03bef7db41d01af6849be9d3c18d084971ec763
sm_samsungLabtop <daverpooh@naver.com>
2019-10-19 12:11:54 +0900

    plot

--- KCDC/GWAS/Gastric/01.frq.plot.R
+++ KCDC/GWAS/Gastric/01.frq.plot.R
@@ -1,19 +1,23 @@
 getwd()
 setwd("C:/Users/user/Desktop/KCDC/Gastric/")
-case <- read.table("CASE_ref.frq",header = T)
-control <- read.table("CONTROL_ref.frq",header = T)
 
-data <- merge(control,case, by = "SNP")
+
+
+
+
+case <- read.table("QC/plotData/CASE.frq",header=T)
+control <- read.table("QC/plotData/CONTROL.frq",header=T)
+data <- merge(control,case,by="SNP")
+#pdf("plot/control&case_frequency.pdf",height = 10,width=10)
+png("plot/control&case_frequency.png")
+
+
 plot(data$MAF.x,data$MAF.y,xlab = "Control",ylab = "Case",main = "Control & Case Frequency",col = "blue")
 abline(a = 0.05,b=1,col = "red",lty = 2)
 abline(a = -0.05,b=1, col = "red",lty = 2)
 #head(data)
 points(data[data$MAF.x-data$MAF.y >= 0.05|data$MAF.x-data$MAF.y<=-0.05,]$MAF.x,
-         data[data$MAF.x-data$MAF.y >= 0.05|data$MAF.x-data$MAF.y<=-0.05,]$MAF.y,
-         col = "red",cex = 1,pch = 1)
+       data[data$MAF.x-data$MAF.y >= 0.05|data$MAF.x-data$MAF.y<=-0.05,]$MAF.y,
+       col = "red",cex = 1,pch = 1)
 
-#points(data[data$MAF.y>=0.05,],col = "blue",cex =1.5,pch = 16)
-?points
-rm <- data[data$MAF.x-data$MAF.y >= 0.05|data$MAF.x-data$MAF.y<=-0.05,]
-
-
+dev.off()
--- KCDC/GWAS/Gastric/02.last_for_each_pop.R
+++ KCDC/GWAS/Gastric/02.last_for_each_pop.R
@@ -1,57 +1,9 @@
-setwd("c:/Users/user/Desktop/KCDC/Gastric/PCA/")
 
-pca <- read.table("PCA.txt",header = T)
-gnomad <- read.table("1000genome_ID.txt",header = F)
-samplegnomad<- read.table("1000GP_Phase3.sample",header = T)
-case<-read.table("CASE_ID.txt",header = F)
-control <-read.table("CONTROL_ID.txt",header = F)
-
-
-colnames(gnomad) <- "name"
-colnames(case) <- "name"
-colnames(control) <- "name"
-
-levels(samplegnomad$GROUP)
-samplegnomad$color <- 0
-#samplegnomad$color[samplegnomad$GROUP == 'SAS',]
-samplegnomad[samplegnomad$GROUP=='SAS',]$color = rgb(1,1,0,0.3)
-samplegnomad[samplegnomad$GROUP=='AFR',]$color = rgb(1,0,1,0.3)
-samplegnomad[samplegnomad$GROUP=='AMR',]$color = rgb(0,1,1,0.3)
-samplegnomad[samplegnomad$GROUP=='EAS',]$color = rgb(0,0,0,0.3)
-samplegnomad[samplegnomad$GROUP=='EUR',]$color = rgb(0,1,0,0.3)
-
-gnomad<- subset(samplegnomad,select = c("ID","color"))
-
-case$name <-as.factor(case$name)
-control$name <-as.factor(control$name)
-#gnomad$color <- rgb(1,0,0,0.3)
-colnames(gnomad) <- c("name","color")
-case$color <- rgb(1,0,0,0.3)
-control$color <- rgb(0,0,1,0.1)
-
-df <- rbind(gnomad,case)
-df <- rbind(df,control)
-colnames(df) <- c("FID","color")
-df <- merge(pca,df,by = "FID")
-color <- c(rgb(1,0,0,0.3),
-         rgb(0,0,1,0.3),
-         rgb(1,1,0,0.3),
-         rgb(1,0,1,0.3),
-         rgb(0,1,1,0.3),
-         rgb(0,0,0,0.3),
-         rgb(0,1,0,0.3))
-list <- c("case","control","SAS","AFR","AMR","EAS","EUR")
-pdf("Last_PCA.pdf",height = 10,width = 10)
-plot(df$PC1,df$PC2,col = df$color,xlab = "PC1",ylab = "PC2",main="Last_PCA"
-     ,cex = 1,pch = 16
-     )
-legend(x = 0 ,y = 0.3,list,col = color,cex = 1,pch = 16)
-dev.off()
 
 ################################################
 
-setwd("c:/Users/user/Desktop/KCDC/Gastric/PCA/")
-pca <- read.table("PCA.txt",header = T)
+setwd("c:/Users/user/Desktop/KCDC/Gastric/QC/plotData/")
+pca <- read.table("all_merge_pca.txt",header = T)
 #gnomad <- read.table("1000genome_ID.txt",header = F)
 samplegnomad<- read.table("1000GP_Phase3.sample",header = T)
 case<-read.table("CASE_ID.txt",header = F)
@@ -74,25 +26,32 @@ df <- rbind(df,control)
 
 df <- merge(df,pca, by = "FID")
 levels(df$GROUP)
-pdf("1kgp.gastric.PCA.pop.pdf",height =10, width = 10)
-plot(df$PC1,df$PC2,col=rgb(0,0,1,0.1),xlab = "PC1",ylab = "PC2",main = "1000gnomad with KCHIP(GASTRIC) PCA",cex = 1, pch =16,cex.main = 3)
-points(df[df$GROUP == "AFR",]$PC1,df[df$GROUP == "AFR",]$PC2,col = rgb(1,0,1,0.3), cex= 1,pch = 16)
-points(df[df$GROUP == "AMR",]$PC1,df[df$GROUP == "AMR",]$PC2,col = rgb(0,1,1,0.3), cex= 1,pch = 16)
-points(df[df$GROUP == "EAS",]$PC1,df[df$GROUP == "EAS",]$PC2,col = rgb(0,0,0,0.3), cex= 1,pch = 16)
-points(df[df$GROUP == "EUR",]$PC1,df[df$GROUP == "EUR",]$PC2,col = rgb(0,1,0,0.3), cex= 1,pch = 16)
-points(df[df$GROUP == "SAS",]$PC1,df[df$GROUP == "SAS",]$PC2,col = rgb(1,1,0,0.3), cex= 1,pch = 16)
-points(df[df$GROUP == "CONTROL",]$PC1,df[df$GROUP == "CONTROL",]$PC2,col = rgb(0,0,1,0.1), cex= 1,pch = 16)
-points(df[df$GROUP == "CASE",]$PC1,df[df$GROUP == "CASE",]$PC2,col = rgb(1,0,0,0.1), cex= 1,pch = 16)
+pdf("../../plot/ethnic.PCA.pdf",height =10, width = 10)
+plot(df$PC1,df$PC2,col = rgb(0,0,1,0.1),xlab = "PC1",ylab = "PC2",main="ethnic_PCA",
+     cex.main = 3,cex = 1,pch = 16
+)
+points(df[df$GROUP == "AFR",]$PC1,df[df$GROUP == "AFR",]$PC2,col = rgb(1,0,1,0.3), cex = 1 , pch = 16)
+points(df[df$GROUP == "AMR",]$PC1,df[df$GROUP == "AMR",]$PC2,col = rgb(0,1,1,0.3), cex = 1 , pch = 16)
+points(df[df$GROUP == "EUR",]$PC1,df[df$GROUP == "EUR",]$PC2,col = rgb(0,1,0,0.3), cex = 1 , pch = 16)
 
-color <- c(rgb(1,0,0,0.7),
-           rgb(0,0,1,0.7),
-           rgb(1,1,0,0.7),
-           rgb(1,0,1,0.7),
-           rgb(0,1,1,0.7),
-           rgb(0,0,0,0.7),
-           rgb(0,1,0,0.7))
-list <- c("case","control","SAS","AFR","AMR","EAS","EUR")
-legend(x = 0 ,y = 0.3,list,col = color,cex = 1,pch = 16)
+points(df[df$GROUP == "SAS",]$PC1,df[df$GROUP == "SAS",]$PC2,col = rgb(1,1,0,0.3), cex = 1 , pch = 16)
+points(df[df$GROUP == "CONTROL",]$PC1,df[df$GROUP == "CONTROL",]$PC2,col = rgb(0,0,1,0.3), cex = 1 , pch = 16)
+points(df[df$GROUP == "CASE",]$PC1,df[df$GROUP == "CASE",]$PC2,col = rgb(1,0,0,0.3), cex = 1 , pch = 16)
+points(df[df$GROUP == "EAS",]$PC1,df[df$GROUP == "EAS",]$PC2,col = rgb(0,0,0,0.3), cex = 1 , pch = 16)
+
+color <- c(
+  rgb(0,0,1,1),
+  rgb(1,0,0,1),
+  rgb(1,1,0,1),
+  rgb(1,0,1,1),
+  rgb(0,1,1,1),
+  rgb(0,0,0,1),
+  rgb(0,1,0,1))
+list <- c("control","case","SAS","AFR","AMR","EAS","EUR")
+
+legend(x = 0 ,y = 0.6,list,col = color,cex = 1,pch = 16)
 dev.off()
+
+
 ##########################################################################3
 
