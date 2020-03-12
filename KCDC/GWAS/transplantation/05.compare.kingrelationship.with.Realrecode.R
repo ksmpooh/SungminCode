9202f1eed9850d9b469fe8b6bc9cafa7a3a6607d
sm_samsungLabtop <daverpooh@naver.com>
2020-03-12 18:55:47 +0900

    m

---
+++ KCDC/GWAS/transplantation/05.compare.kingrelationship.with.Realrecode.R
@@ -0,0 +1,89 @@
+library(readr)
+library(dplyr)
+setwd("c:/Users/user/Desktop/KCDC/transplantation/")
+real <- readr::read_csv("final_sample_info/pairtable/KOTRY_KCHIP_ID_full_20200306.csv",col_names = T)
+real <- as.data.frame(real)
+head(real)
+
+table(real$Rela_Pair)
+
+
+
+real_pair <- real[,c(1,8,9,10,11,12,19,20,21,22)]
+head(real_pair)
+colnames(real_pair)
+
+#real_pair <- real_pair[,c(1,4,6,10)]
+real_pair <- real_pair[,c(1,6)]
+real_pair <-na.omit(real_pair)
+
+result <- read.table("2nd/all.king/2ndkingResult.withType.txt",header = T)
+head(result)
+result_pair <- result[,c(1,3)]
+head(result_pair)
+
+head(real_pair)
+colnames(real_pair) <- c("FID1","FID2")
+#colnames(real_pair)[1] <- "FID1"
+#colnames(real_pair)[3] <- "FID2"
+real_pair$FID1 <- as.factor(real_pair$FID1)
+real_pair$FID2 <- as.factor(real_pair$FID2)
+head(result_pair)
+
+
+str(real_pair)
+str(result_pair)
+
+df <- real_pair %>%anti_join(result_pair,by=c("FID1","FID2"))
+head(df)
+colnames(df) <- c("FID2","FID1")
+#colnames(real_pair)[1] <- "FID2"
+#colnames(real_pair)[3] <- "FID1"
+
+df <- df %>%anti_join(result_pair,by=c("FID2","FID1"))
+head(df)
+colnames(df) <- c("FID1","FID2")
+
+head(df)
+
+colnames(df) <- c("KCHIP_ID","KCHIP_ID1")
+head(real)
+
+
+
+rmlist <- read.table("final_sample_info/last.sample.info.txt",header = T)
+
+head(rmlist)
+table(rmlist$state)
+rmlist <- rmlist[rmlist$state == "remove",]
+colnames(rmlist)[1]<-"KCHIP_ID"
+
+df <- df %>% anti_join(rmlist,by = "KCHIP_ID")
+
+
+colnames(rmlist)[1]<-"KCHIP_ID1"
+
+df <- df %>% anti_join(rmlist,by = "KCHIP_ID1")
+
+
+df <- merge(df,real,by = "KCHIP_ID")
+df <- df[,c(-2)]
+colnames(df)[12] <- "KCHIP_ID1"
+head(df)
+
+
+write.table(df,"final_sample_info/pairtable/not.predicted.by.king.txt",col.names = T,row.names = F,quote = F)
+head(df)
+table(df$Rela_Pair)
+table(df$Rela_Pair1)
+
+table(real$Rela_Pair)
+df[df$Rela_Pair == 1,]$KCHIP_ID
+
+
+
+
+a<-df$KCHIP_ID
+b <- factor(df$KCHIP_ID1)
+c <- rbind(a,b)
+head(c)
