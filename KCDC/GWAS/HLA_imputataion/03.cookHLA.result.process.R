5fc7b97ab7b18a6a8d5fe3c1f61b6ecbc9656458
sm_samsungLabtop <daverpooh@naver.com>
2020-03-18 16:23:06 +0900

    hla imputation result comparing data processing final

--- KCDC/GWAS/HLA_imputataion/03.cookHLA.result.process.R
+++ KCDC/GWAS/HLA_imputataion/03.cookHLA.result.process.R
@@ -122,21 +122,139 @@ df <-df[is.na(df$DRB3),]
 df <- df[,c(2,3,4,5,6,8,9)]
 colnames(df)[1] <- "ID"
 
+df[,2:ncol(df)] <- sapply(df[,2:ncol(df)],as.integer)
+str(df)
+
 ref <- read.table("../../transplantation/HLAtyping/HLA_JG_2DGT_imputed.txt",header = T)
 
 a <- merge(df[,c(1,2,3)],ref[,c(1,2,3)],by = "ID",all.x = T)
 b <- merge(df[,c(1,4,5)],ref[,c(1,4,5)],by = "ID",all.x = T)
 drb <- merge(df[,c(1,6,7)],ref[,c(1,6,7)],by = "ID",all.x = T)
 
+colnames(a) <- c("ID","sm.A1","sm.A2","yj.A1","yj.A2")
+colnames(b) <- c("ID","sm.B1","sm.B2","yj.B1","yj.B2")
+colnames(drb) <- c("ID","sm.DRB1","sm.DRB2","yj.DRB1","yj.DRB2")
+head(a)
+dim(a)
+dim(b)
+dim(drb)
 
 write.table(a,"HLA_imputation_A_allele_2d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
 write.table(b,"HLA_imputation_B_allele_2d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
 write.table(drb,"HLA_imputation_DRB_allele_2d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
 
 #write.table(df[,c(2,3,4,5,6,8,9)],"HLA_imptation_2d_without_3_alleles.txt",col.names = T,row.names = F,quote = F)
-head(df[,c(2,3,4,5,6,8,9)])
-df[1:5,]
 
-##DRB3 = 16, B3 = 7
-6589 - 16 - 7
-intersect(a$FID,b$FID)
+
+
+#############################################################################################
+
+
+DRB_fd <- hla.subset(DRB,4)
+head(DRB_fd)
+DRB_fd <- hla.find(DRB_fd,"DRB")
+
+
+DRB_fd <- DRB_fd[,c(1,2,ncol(DRB_fd)-2,ncol(DRB_fd)-1,ncol(DRB_fd))]
+head(DRB_fd)
+table(DRB_fd$DRB3)
+
+#a <- DRB_fd[!is.na(DRB_fd$DRB3),]
+#write.table(a,"HLAimputation.3alleles.in.DRB.4digit.txt",col.names = T,row.names = F,quote = F)
+
+DRB_fd <- DRB_fd[is.na(DRB_fd$DRB3),]
+
+A_fd <- hla.subset(A,4)
+A_fd <- hla.find(A_fd,"A")
+A_fd <- A_fd[,c(1,2,ncol(A_fd)-1,ncol(A_fd))]
+head(A_fd)
+
+
+B_fd <- hla.subset(B,4)
+B_fd <- hla.find(B_fd,"B")
+head(B_fd)
+B_fd <- B_fd[,c(1,2,ncol(B_fd)-1,ncol(B_fd))]
+
+df <- merge(A_fd,B_fd)
+df <- merge(df,DRB_fd)
+
+
+df <-df[is.na(df$DRB3),]
+head(df)
+df <- subset(df,select = -c(DRB3,FID))
+
+colnames(df)[1] <- "ID"
+
+df[,2:ncol(df)] <- sapply(df[,2:ncol(df)],as.integer)
+str(df)
+
+#ref <- read.table("../../transplantation/HLAtyping/HLA_JG_4DGT_imputed.txt",header = T)
+ref <- read.csv("../../transplantation/HLAtyping/HLA_JG_4DGT_imputed.csv",header = T)
+head(ref)
+
+a <- merge(df[,c(1,2,3)],ref[,c(1,2,3)],by = "ID",all.x = T)
+b <- merge(df[,c(1,4,5)],ref[,c(1,4,5)],by = "ID",all.x = T)
+drb <- merge(df[,c(1,6,7)],ref[,c(1,6,7)],by = "ID",all.x = T)
+
+colnames(a) <- c("ID","sm.A1","sm.A2","yj.A1","yj.A2")
+colnames(b) <- c("ID","sm.B1","sm.B2","yj.B1","yj.B2")
+colnames(drb) <- c("ID","sm.DRB1","sm.DRB2","yj.DRB1","yj.DRB2")
+head(a)
+dim(a)
+dim(b)
+dim(drb)
+
+write.table(a,"HLA_imputation_A_allele_4d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
+write.table(b,"HLA_imputation_B_allele_4d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
+write.table(drb,"HLA_imputation_DRB_allele_4d_without.3.allele.txt",col.names = T,row.names = F,quote = F,sep = '\t')
+
+
+##################python data processing 후 merge 작업
+setwd("c:/Users/user/Desktop/KCDC/HLAimputation/Result/")
+final_merge <- function(digit){
+  
+  a <- paste0("HLA.imputation.A_allele.",as.character(digit),"digit.without.3.allele.compare.sm.and.yj.txt")
+  b <- paste0("HLA.imputation.B_allele.",as.character(digit),"digit.without.3.allele.compare.sm.and.yj.txt")
+  drb <- paste0("HLA.imputation.DRB_allele.",as.character(digit),"digit.without.3.allele.compare.sm.and.yj.txt")
+  
+  a<-read.table(a,header = T)
+  b<-read.table(b,header = T)
+  drb<-read.table(drb,header = T)
+  
+  df <- merge(a,b,by = "ID")
+  df <- merge(df,drb,by = "ID")
+  out = paste0("HLA.imputation.",as.character(digit),"digit.result.compare.sm.with.yj.csv")
+  #write.csv(df,out,row.names = F,quote = F)
+  return(df)
+}
+
+df <- final_merge(4)
+head(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################처음에 했던 merge
+a <- read.table("HLA.imputation.A_allele.2digit.without.3.allele.compare.sm.and.yj.txt",header = T)
+b <- read.table("HLA.imputation.B_allele.2digit.without.3.allele.compare.sm.and.yj.txt",header = T)
+drb <- read.table("HLA.imputation.DRB_allele.2digit.without.3.allele.compare.sm.and.yj.txt",header = T)
+head(a)
+head(b)
+head(drb)
+
+
+df <- merge(a,b,by = "ID")
+df <- merge(df,drb,by = "ID")
+head(df)
+write.csv(df,"HLA.imputation.2digit.result.compare.sm.with.yj.csv",col.names = T,row.names = F,quote = F)
+
+
