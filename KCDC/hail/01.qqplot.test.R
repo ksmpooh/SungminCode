b2119499f77cbe7a7118749b009be4a68150d9d6
sm_samsungLabtop <daverpooh@naver.com>
2021-06-11 16:20:32 +0900

    plot

---
+++ KCDC/hail/01.qqplot.test.R
@@ -0,0 +1,28 @@
+library(stringr)
+setwd("c:/Users/user/Desktop/KCDC/hail/test/")
+d1 <- read.table("hail/gaws.output.T2D.tsv",header = T)
+head(d1)
+d1 <- d1[,c(1,3,4,5)]
+d2 <- read.table("epacts/test.chr22.merge.list.txt",header = T)
+head(d2)
+d2 <- d2[,c(9,10,12)]
+head(d1)
+colnames(d1) <- c("locus","hail.BETA","hail.CHISQ","hail.PVALUE")
+head(d2)
+colnames(d2) <- c("epacts.PVALUE","epacts.BETA","epacts.CHISQ")
+
+
+d <- cbind(d1,d2)
+head(d)
+d$p.check = 0
+d[d$epacts.PVALUE <0.05,]$p.check = 1
+#d <- unique.data.frame(d)
+library(ggplot2)
+ggplot(main = "association Result : hail.beta vs epacts.beta",d,aes(x=hail.BETA,y=epacts.BETA,color = ifelse(p.check==1,"red","darkgray"))) +
+  geom_point() +
+  scale_color_manual(guide=FALSE, values=c("red","darkgray"))
+
+
+ggplot(main = "association Result : hail.p-value vs epacts.p-value",d,aes(x=-log10(hail.PVALUE),y=-log10(epacts.PVALUE),color = ifelse(p.check==1,"red","darkgray"))) +
+  geom_point() +
+  scale_color_manual(guide=FALSE, values=c("red","darkgray"))
