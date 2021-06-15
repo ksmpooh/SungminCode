fe32ebc56974001ed3fcb30631136a5558af6654
sm_samsungLabtop <daverpooh@naver.com>
2021-06-15 11:27:53 +0900

    sm

--- KCDC/hail/01.qqplot.test.R
+++ KCDC/hail/01.qqplot.test.R
@@ -16,14 +16,64 @@ d <- cbind(d1,d2)
 head(d)
 d$p.check = 0
 d[d$epacts.PVALUE <0.05,]$p.check = 1
+d$p.check <- as.factor(d$p.check)
 #d <- unique.data.frame(d)
 library(ggplot2)
-ggplot(main = "association Result : hail.beta vs epacts.beta",d,aes(x=hail.BETA,y=epacts.BETA,color = ifelse(p.check==1,"red","darkgray"))) +
-  geom_point() +
+ggplot(d,aes(x=hail.BETA,y=epacts.BETA,fill = p.check,color = ifelse(p.check==1,"red","darkgray"))) +
+#ggplot(d,aes(x=hail.BETA,y=epacs.BETA,fill = p.check)) +
+  ggtitle("association Result : hail.beta vs epacts.beta") +
+  geom_point() + 
+  theme(plot.title = element_text(hjust = 0.5),legend.title = element_text()) +
+  #guides(fill = guide_legend(title = "epacts P.value")) +
+  scale_fill_discrete(name = "epacts P.value",label = c("<0.05",">=0.05")) + 
+#  scale_fill_manual(values = c("#FF0000","#a9a9a9")) 
+#  scale_fill_gradientn(colours = c("red","darkgrey")) +
   scale_color_manual(guide=FALSE, values=c("red","darkgray"))
 
+cor.test(d$hail.BETA,d$epacts.BETA)
 
 ggplot(d,aes(x=-log10(hail.PVALUE),y=-log10(epacts.PVALUE),color = ifelse(p.check==1,"red","darkgray"))) +
   ggtitle( "Association Result Compare : hail.p-value vs epacts.p-value") +
   geom_point() + theme(plot.title = element_text(hjust = 0.5)) +
   scale_color_manual(guide=FALSE, values=c("red","darkgray"))
+
+
+
+
+ggplot(d,aes(x=hail.BETA,y=epacts.BETA,fill = p.check,color = ifelse(p.check=="1","red","darkgray"))) +
+#  scale_color_manual(guide=FALSE, values=c("darkgray","red")) + 
+  scale_color_manual(guide=FALSE, values=c("darkgray","red")) + 
+  #ggplot(d,aes(x=hail.BETA,y=epacs.BETA,fill = p.check)) +
+  ggtitle("association Result : hail.beta vs epacts.beta") +
+  geom_point()
+  theme(plot.title = element_text(hjust = 0.5),legend.title = element_text()) +
+  labs(fill = "epacts.P.value") + 
+  scale_fill_discrete(labels = c(">=0.05","<0.05")) 
+#  scale_colour_manual(values = c("#FF0000","#a9a9a9")) 
+#  scale_colour_manual(values = c("darkgrey","red")) 
+
+# guides(fill = guide_legend(title = "epacts P.value"))
+  scale_fill_discrete(name = "epacts P.value",label = c("<0.05",">=0.05"))
+  scale_color_manual(values = c("#FF0000","#a9a9a9")) 
+  #  scale_fill_gradientn(colours = c("red","darkgrey")) +
+ scale_color_manual(guide=FALSE, values=c("red","darkgray"))
+
+ 
+ 
+ 
+ 
+summary(d$p.check)
+plot(d$hail.BETA,d$epacts.BETA,main = "association Result : hail.beta vs epacts.beta"
+     ,xlab = "hail.Beta",ylab = "epact.Beta"
+     ,col = ifelse(d$p.check==0,rgb(0.5,0.5,0.5,0.2),rgb(1,0,0,0.5))
+     #,col = rgb(0.6,0.6,0.6,0.3)
+     ,pch =16)
+legend("bottomright",title = "epacts p.value",legend = c("<0.05",">=0.05"),col = c("red","darkgray"),pch = 16,box.lty = 0)
+
+
+hist(abs(d$epacts.BETA - d$hail.BETA))
+
+plot(-log10(d$hail.PVALUE),-log10(d$epacts.PVALUE),main = "association Result : hail.p value vs epacts.p value"
+     ,xlab = "-log10(hail p-value)",ylab = "-log10(epacts p-value)"
+     ,col = ifelse(d$p.check==0,rgb(0.5,0.5,0.5,0.5),rgb(1,0,0,0.5)))
+legend("bottomright",title = "epacts p.value",legend = c("<0.05",">=0.05"),col = c("red","darkgray"),pch = 16,box.lty = 0)
