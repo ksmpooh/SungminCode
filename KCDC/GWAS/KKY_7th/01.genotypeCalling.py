2b37b68237160778cae42e773c4408a8c2d33d89
ksmpooh <daverpooh@naver.com>
2021-07-21 18:05:50 +0900

    sm

--- KCDC/GWAS/KKY_7th/01.genotypeCalling.py
+++ KCDC/GWAS/KKY_7th/01.genotypeCalling.py
@@ -9,28 +9,39 @@
 ~/Downloads/apt-1.19.0-x86_64-intel-linux/bin/apt-genotype-axiom 
 --analysis-files-path /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis 
 --arg-file /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/Axiom_KORV1_1_96orMore_Step2.r1.apt-genotype-axiom.AxiomGT1.apt2.xml 
---dual-channel-normalization true --cel-files /BDATA/smkim/JG/00.rawData/sample_info/KR_2nd_cel_file_list2.txt 
---summaries --write-models --out-dir /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/
+--dual-channel-normalization true --cel-files /DATA/smkim/KKY/01.GenotypeCallingcel_files.txt
+--summaries --write-models --out-dir ./
 
 ##SNPolisher
 ~/Downloads/apt-1.19.0-x86_64-intel-linux/bin/ps-metrics 
---posterior-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.snp-posteriors.txt 
---call-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.calls.txt 
---metrics-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.out.txt
+--posterior-file ./AxiomGT1.snp-posteriors.txt 
+--call-file ./AxiomGT1.calls.txt 
+--metrics-file ./AxiomGT1.out.txt
 
 
 ~/Downloads/apt-1.19.0-x86_64-intel-linux/bin/ps-classification 
 --species-type human 
---metrics-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.out.txt 
---output-dir /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/
+--metrics-file ./AxiomGT1.out.txt 
+--output-dir ./
 
 ~/Downloads/apt-1.19.0-x86_64-intel-linux/bin/ps-classification-supplemental 
---performance-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/Ps.performance.txt 
---summary-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.summary.txt 
---call-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.calls.txt --posterior-file /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/AxiomGT1.snp-posteriors.txt 
---output-dir /BDATA/smkim/JG/01.1stgenocall/OUTPUTs/KR.2nd/
+--performance-file ./Ps.performance.txt 
+--summary-file ./AxiomGT1.summary.txt 
+--call-file ./AxiomGT1.calls.txt --posterior-file ./AxiomGT1.snp-posteriors.txt 
+--output-dir ./
 
 ##v2
+/home/genome/Downloads/apt_2.11.3_linux_64_bit_x86_binaries/bin/apt-genotype-axiom \
+--analysis-files-path /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/ \
+--arg-file /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/Axiom_KORV1_1_96orMore_Step2.r1.apt-genotype-axiom.AxiomGT1.apt2.xml \
+--out-dir ./APT_advnorm/ \
+--log-file ./APT_advnorm/apt-genotype-axiom.log \
+--allele-summaries true --snp-posteriors-output true --dual-channel-normalization true \
+--cel-files ./cel_file_list.txt \
+--genotyping-node:snp-priors-input-file /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/Axiom_KORV1_1.r1.AxiomGT1.models \
+--artifact-reduction-trustcheck true \
+--artifact-reduction-output-trustcheck true
+
 /home/genome/Downloads/apt_2.11.3_linux_64_bit_x86_binaries/bin/ps-metrics \
 --metrics-file ./APT_advnorm/metrics.txt \
 --log-file ./APT_advnorm/PS_Metrics.log \
@@ -53,15 +64,29 @@ import os,glob
 
 def main():
     toolDir = "~/Downloads/apt_2.11.3_linux_64_bit_x86_binaries/bin/"
-    libraryDir = "~/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis"
+    libraryDir = "~/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/"
     wDir = "/DATA/smkim/KKY/01.GenotypeCalling/"
     inDir = wDir + "INPUTs/"
     outDir = wDir + "OUTPUTs/"
     celfiles = inDir + "cel_files.txt"
     #callDir = 
     #os.system("%sapt-genotype-axiom --analysis-files-path %s --arg-file %s/Axiom_KORV1_1_96orMore_Step2.r1.apt-genotype-axiom.AxiomGT1.apt2.xml --dual-channel-normalization true --cel-files %s --summaries --write-models --out-dir %s"%(toolDir,libraryDir,libraryDir,celfiles,outDir))
-    print("%sapt-genotype-axiom --analysis-files-path %s --arg-file %s/Axiom_KORV1_1_96orMore_Step2.r1.apt-genotype-axiom.AxiomGT1.apt2.xml --dual-channel-normalization true --cel-files %s --summaries --write-models --out-dir %s\n"%(toolDir,libraryDir,libraryDir,celfiles,outDir))
-    print("%sps-metrics --posterior-file %sAxiomGT1.snp-posteriors.txt  --call-file %sAxiomGT1.calls.txt  --metrics-file %sAxiomGT1.out.txt\n"%(toolDir,outDir,outDir,outDir))
+    print("%sapt-genotype-axiom --analysis-files-path %s \
+        --arg-file %sAxiom_KORV1_1_96orMore_Step2.r1.apt-genotype-axiom.AxiomGT1.apt2.xml \
+            --allele-summaries true --snp-posteriors-output true --dual-channel-normalization true \
+                --artifact-reduction-trustcheck true --artifact-reduction-output-trustcheck true \
+                --cel-files %s \
+                --summaries --write-models \
+                    --genotyping-node:snp-priors-input-file /home/genome/Downloads/apt-v1.1/Axiom_KORV1.1_Analysis/Axiom_KORV1_1.r1.AxiomGT1.models \
+                    --out-dir %s\n"%(toolDir,libraryDir,libraryDir,celfiles,outDir))
+    #print("%sps-metrics --posterior-file %sAxiomGT1.snp-posteriors.txt  --call-file %sAxiomGT1.calls.txt  --metrics-file %sAxiomGT1.out.txt\n"%(toolDir,outDir,outDir,outDir))
+    print("%sps-metrics \
+        --metrics-file %smetrics.txt \
+        --log-file %sPS_Metrics.log \
+        --posterior-file %sAxiomGT1.snp-posteriors.txt \
+        --summary-file %sAxiomGT1.summary.txt \
+        --report-file %sAxiomGT1.report.txt \
+        --call-file %sAxiomGT1.calls.txt \
+        --special-snps %sAxiom_KORV1_1.r1.specialSNPs"%(toolDir,outDir,outDir,outDir,outDir,outDir,libraryDir))
 
-
-main()    
+main()
--- KCDC/GWAS/UK/01_2.merge.py
+++ KCDC/GWAS/UK/01_2.merge.py
@@ -2,12 +2,29 @@ import os,glob
 #phenoList = ["ALT_logz", "GLU_inv", "HbA1c_inv", "AST_logz", "GGT_logz"]
 phenoList = ["GLU_inv", "HbA1c_inv", "DBP.nRES", "SBP.nRES","PP.nRES","MAP.nRES"]
 def main():
-    outDir = "merge/"
+    outDir = "MERGE/"
     os.system("mkdir "+outDir)
     for pheno in phenoList:
         #ALT_logz.onlyID.txt
         #no.covariates.header.txt
         #os.system("cat *%s*.txt > %s%s.ID.txt"%(pheno,outDir,pheno))
-        os.system("cat *%s*.txt | grep -v \"rsid\" | awk \'{print $1,$3,$4,$21}' > ./merge/UKB_%s_all.MERGE.for.manhattan.txt"%(pheno,pheno))
+        #os.system("cat *%s*.txt | grep -v \"rsid\" | awk \'{print $1,$3,$4,$21}' > ./merge/UKB_%s_all.MERGE.for.manhattan.txt"%(pheno,pheno))
+        output = outDir + "UKB_%s_ALL_MERGE.txt"%pheno
+        header = "no.covariates.header.txt"
+        os.system("cp %s %s"%(header,output))
+        os.system("cat *%s*.txt | grep -v \"rsid\" >> %s"%(pheno,output))
 
 main()
+
+
+#ukb57705_imp_v3_s487283_Liver_20210422.FINAL.sample
+#ukb57705_imp_v3_s487283_GLY_20210422.FINAL.sample
+#ukb57705_imp_v3_s487283_HT_20210517.sample
+1                 3           4                                                               9                                                                                                 19
+#alternate_ids rsid chromosome position alleleA alleleB index average_maximum_posterior_call info cohort_1_AA cohort_1_AB cohort_1_BB cohort_1_NULL all_AA all_AB all_BB all_NULL all_total all_maf missing_data_proportion frequentist_add_pvalue frequentist_add_info frequentist_add_beta_1 frequentist_add_se_1 comment
+def forMan():
+    outDir = "forManhattanPlot/"
+    for pheno in phenoList:
+        input =  "UKB_%s_ALL_MERGE.txt"%pheno
+        output = oudtDir + "UKB_%s_ALL_MERGE_for.manhattan.txt"%pheno
+        os.system("grep -v \"rsid\" %s | awk \'$9 >= 0.8 & $19 >= 0.01{print $1,$3,$4,$21}\' > %s"%(input,output))
