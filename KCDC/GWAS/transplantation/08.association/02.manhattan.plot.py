1f19d2a08b9d7f4738d5a82a92e198e55743bb81
ksmpooh <daverpooh@naver.com>
2021-09-13 10:25:29 +0900

    sm

--- KCDC/GWAS/transplantation/08.association/02.manhattan.plot.py
+++ KCDC/GWAS/transplantation/08.association/02.manhattan.plot.py
@@ -1,11 +1,11 @@
 import os,glob
 
-wDir = "/BDATA/smkim/JG/08.asso/OUTPUTs/"
+wDir = "/BDATA/smkim/JG_2020/08.asso/OUTPUTs/"
 
 mergeDir = wDir + "01_1.merge/"
 plotDir = mergeDir + "plot/"
 os.system("mkdir " + plotDir)
-plotsh = "/BDATA/smkim/JG/08.asso/SCRIPTs/03.ploting.R"
+#plotsh = "/BDATA/smkim/JG/08.asso/SCRIPTs/03.ploting.R"
 #Three args : [input.txt] [output]
 
 def main():
@@ -15,9 +15,9 @@ def main():
         print("df : " + df)
         datain = df
         os.system("grep -v ^# %s |awk '{print $4,$1,$2,$9}' > %s"%(datain, datain.replace(".txt",".manhattan.form.txt"))) 
-        plotout = datain.replace(mergeDir,plotDir).replace(".txt","")
-        datain = datain.replace(".txt",".manhattan.form.txt")
-        os.system("Rscript --vanilla %s %s %s"%(plotsh,datain,plotout))
+#        plotout = datain.replace(mergeDir,plotDir).replace(".txt","")
+#        datain = datain.replace(".txt",".manhattan.form.txt")
+#        os.system("Rscript --vanilla %s %s %s"%(plotsh,datain,plotout))
 
 main()
 
