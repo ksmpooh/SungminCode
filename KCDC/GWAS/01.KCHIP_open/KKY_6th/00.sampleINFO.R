4d3836850881a87648988bfc27c8012d0d2fbf63
ksmpooh <daverpooh@naver.com>
2022-05-24 17:39:09 +0900

    sm

--- KCDC/GWAS/01.KCHIP_open/KKY_6th/00.sampleINFO.R
+++ KCDC/GWAS/01.KCHIP_open/KKY_6th/00.sampleINFO.R
@@ -7,20 +7,97 @@ setwd("~/Desktop/KCDC/KKY/00.sampleInfo/")
 df <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx")
 
 ori <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx",sheet = 2)
-Acc <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx",sheet = 3)
-Rep <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx",sheet = 4)
+Rep <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx",sheet = 3)
+Acc <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과_bysm.xlsx",sheet = 4)
+
+ref <- read.table("2020.all.teragen.KKY.info_20220311.txt",header = T)
+ref$BATCH <- paste0("KNHANES_",ref$th)
+head(ref)
+
+mzlist1 <- read.table("2020_tera/KBA_Blind_matchingTable.txt")
+mzlist2 <- read.table("2020_tera/KBA_TPMRA_matchingtable_v2.txt")
+
+head(mzlist)
+str_split_fixed(str_split_fixed(mzlist$V1,"_",6)[,6],"\\.",2)[,1]
+
+mzlist1 <- mzlist1 %>% select(V1,V3) %>% 
+  mutate(IID=str_split_fixed(str_split_fixed(V1,"_",6)[,6],"\\.",2)[,1],BLINDID=str_split_fixed(str_split_fixed(V3,"_",6)[,6],"\\.",2)[,1]) %>%
+  rename("KCHIP_CEL" = V1, "BLIND_CEL" = V3)
+
+mzlist2 <- mzlist2 %>% select(V1,V3) %>% 
+  mutate(IID=str_split_fixed(str_split_fixed(V1,"_",6)[,6],"\\.",2)[,1],BLINDID=str_split_fixed(str_split_fixed(V3,"_",5)[,5],"\\.",2)[,1]) %>%
+  rename("KCHIP_CEL" = V1, "BLIND_CEL" = V3)
+
+head(mzlist1)
+head(mzlist2)
+mzlist <- rbind(mzlist1,mzlist2)
 
 head(ori)
-ori %>% select(id,probeset_id,set,gender) %>% 
-  mutate(FID = id,BLINDID = "-",COHORT = "KNHANES",YEAR = "2020",DATATYPE="KORV1.1",INFO = "-",BATCH ="KNHANES_6th") %>%
+a <- ori %>% select(id,probeset_id,set,gender) %>% 
+  mutate(FID = id,COHORT = "KNHANES",YEAR = "2020",DATATYPE="KORV1.1",INFO = "-") %>%
   rename('IID' = id,'CEL' = probeset_id,"SET" = set,"GENDER" = gender) %>% 
+  merge(ref[,c(1,3)],by.x = "FID",by.y = "id") %>%
+  full_join(mzlist[,c("IID","BLINDID")]) %>%
+  mutate(BLINDID = ifelse(is.na(BLINDID),"-",BLINDID)) %>%
   select(IID,FID,CEL,SET,GENDER,BLINDID,COHORT,YEAR,DATATYPE,INFO,BATCH) %>%
-  head()
+  mutate(SET = paste0("DL",str_replace(SET,"'","")))
+
+table(a$BLINDID)
+head(a)
+a %>% filter(FID == "NIH20C2841404")
+mzlist %>% filter(IID == "NIH20C2841404")
+head(mzlist)
+
+head(mzlist)
+colnames(mzlist)[3:4] <- c("BLINDID","IID")
+b <-Rep %>% select(id,probeset_id,set,gender) %>% 
+  mutate(FID = id,COHORT = "KNHANES",YEAR = "2020",DATATYPE="KORV1.1",INFO = "Reproducibility") %>%
+  rename('IID' = id,'CEL' = probeset_id,"SET" = set,"GENDER" = gender) %>% 
+  inner_join(mzlist[,c("IID","BLINDID")]) %>%
+  merge(ref[,c(1,3)],by.x = "BLINDID",by.y = "id") %>% 
+  select(IID,FID,CEL,SET,GENDER,BLINDID,COHORT,YEAR,DATATYPE,INFO,BATCH) %>%
+  mutate(SET = paste0("DL",str_replace(SET,"'","")))
+
+head(b)
+
+c <- Acc %>% select(id,probeset_id,set,apt_probeset_genotype_gender) %>% 
+  mutate(FID = id,COHORT = "KNHANES",YEAR = "2020",DATATYPE="TPMRA",INFO = "Accuracy") %>%
+  rename('IID' = id,'CEL' = probeset_id,"SET" = set,"GENDER" = apt_probeset_genotype_gender) %>% 
+  inner_join(mzlist[,c("IID","BLINDID")]) %>%
+  merge(ref[,c(1,3)],by.x = "BLINDID",by.y = "id") %>% 
+  select(IID,FID,CEL,SET,GENDER,BLINDID,COHORT,YEAR,DATATYPE,INFO,BATCH) %>%
+  mutate(SET = paste0("DL",str_replace(SET,"'","")))
 
 
+head(c)
+head(mzlist)
+head(ref)
 df <- read_excel("2020년도 한국인칩을 이용한 국민건강영양조사 참여자 유전체 정보 생산6136건 결과.xlsx",sheet = 3)
 head(df);dim(df)
+head(a)
+df <- read.table("2020_tera/gender_Mismatch.txt",header = F)
+gender_ref <- read.table("2020_tera/gender.mismatch.id.txt",header =F) %>% 
+  mutate(BATCH=ifelse(V1 == "2013" || V1 =="2014" ||V1 == "2015","KNHANES_6th","KNHANES_7th")) %>%
+  rename("FID" = V2)
+head(gender_ref)
 
 
-df <- read.t
+head(df)
+d<-df %>% mutate(COHORT = "KNHANES",YEAR = "2020",DATATYPE="KORV1.1",INFO = "gender_discrepancy_fail",BLINDID="-",GENDER="-") %>%
+  mutate(FID = str_split_fixed(str_split_fixed(V1,"_",6)[,6],"\\.",2)[,1]) %>%
+  mutate(IID = FID) %>%
+  rename("CEL" = V1) %>%
+  inner_join(gender_ref[,c(2,3)]) %>%
+  mutate(SET = str_split_fixed(CEL,"_",6)[,4]) %>%
+  mutate(SET = paste0("DL",str_replace(SET,"'",""))) %>%
+  select(IID,FID,CEL,SET,GENDER,BLINDID,COHORT,YEAR,DATATYPE,INFO,BATCH)
+  
+out <- rbind(a,b)
+out <- rbind(out,c)    
+out <- rbind(out,d)
+dim(out)
+head(out)
 
+
+library(writexl)
+write_xlsx(out,"2020_tera/2020.PRO.FINAL.info.xlsx")
--- KCDC/HLAsequencing/long-read/01.bamQC.boxplot_forlong.R
+++ KCDC/HLAsequencing/long-read/01.bamQC.boxplot_forlong.R
@@ -1,51 +1,102 @@
+## long read bam box plot
 
-setwd("~/Desktop/KCDC/long_read/2022/short-read/01.bam.stats/")
+#HLA.Longread.Seq.NIH19KT0745_mapped.Q20.bam.flagstats HLA.Longread.Seq.NIH19KT3809_mapped.Q20.bam.flagstats
+#HLA.Longread.Seq.NIH19KT0745_mapped.Q20.bam.stats     HLA.Longread.Seq.NIH19KT3809_mapped.Q20.bam.stats
+#HLA.Longread.Seq.NIH19KT0745_mapped.bam.coverage
+setwd("~/Desktop/KCDC/long_read/2022/long-read/01.bam.stats/")
 library(tidyverse)
-ls_dedup <- system("ls | grep dedup.bam.coverage", intern = T)
-ls_nodup <- system("ls | grep sorted.bam.coverage", intern = T)
-head(ls_nodup)
 
-#sample_id <- unlist(strsplit(ls_dedup[1], split = "\\.", perl=T))[4]
-#sample_id
+
+
+ls_Q20 <- system("ls | grep Q20.bam.coverage", intern = T)
+ls_noQ <- system("ls | grep mapped.bam.coverage", intern = T)
+head(ls_Q20)
+
+sample_id <- str_replace(unlist(strsplit(ls_Q20[1], split = "\\.", perl=T))[4],"_mapped","")
+sample_id
 #file_suffix <- ".subreads.gt-cl.ext.stats"
-#file_suffix <- ".merge.stats"
-###
+file_suffix <- ".merge.stats"
 ###
 #unlist(strsplit(i, split = "\\.", perl=T))[4]
-df_dedup = data.frame()
+df_Q20 = data.frame()
 header = c("rname","startpos","endpos","numreads","covbases","coverage","meandepth","meanbaseq","meanmapq")
-for (i in ls_dedup) {
+for (i in ls_Q20) {
   tmp <- read.table(paste0(i))
-  sample_id <- unlist(strsplit(i, split = "\\.", perl=T))[4]
+  #sample_id <- str_replace(unlist(strsplit(i, split = "\\.", perl=T))[4],"_mapped","")
   colnames(tmp)<-header
-  tmp$ID <- sample_id
-  df_dedup <- rbind(df_dedup,tmp)
+  #tmp$ID <- sample_id
+  tmp$ID <- str_replace(i,".coverage","")
+  df_Q20 <- rbind(df_Q20,tmp)
 }
 
-df_nodup = data.frame()
+
+head(df_Q20)
+
+df_noQ = data.frame()
 header = c("rname","startpos","endpos","numreads","covbases","coverage","meandepth","meanbaseq","meanmapq")
-for (i in ls_nodup) {
+for (i in ls_noQ) {
   tmp <- read.table(paste0(i))
-  sample_id <- unlist(strsplit(i, split = "\\.", perl=T))[4]
+  #sample_id <- str_replace(unlist(strsplit(i, split = "\\.", perl=T))[4],"_mapped","")
   colnames(tmp)<-header
-  tmp$ID <- sample_id
-  df_nodup <- rbind(df_nodup,tmp)
+  #tmp$ID <- sample_id
+  tmp$ID <- str_replace(i,".coverage","")
+  df_noQ <- rbind(df_noQ,tmp)
 }
 
-df_dedup$type <- 'MarkDup'
-df_nodup$type <- "BeforeRmDup"
 
-df <- rbind(df_dedup,df_nodup)
+
+
+
+
+df_Q20$type <- 'FilterQ20'
+df_noQ$type <- "BeforeFilter"
+
+df <- rbind(df_Q20,df_noQ)
 head(df)
-par(mfrow = c(2, 3))
-dev.off()
-for (i in 4:9) {
-#  print(i)
-  ggplot(df,aes(y=colnames(df)[i],x=type,fill=type)) + 
-    geom_boxplot() +
-    theme(legend.position = "none")
-  
-}
+
+av_length <- read.table("../01.bam.average.length.txt")
+av_length<-av_length %>% mutate(ID = str_replace(V1,".stats:","")) %>% #head()
+  rename("mean_readLength" = V2) %>% select(ID,mean_readLength)
+
+head(av_length)
+head(df)
+df <- df %>% inner_join(av_length)
+
+ggplot(df,aes(x=mean_readLength,fill= type)) +
+  geom_histogram( bins = 30) + 
+  xlab("Mean Read Length") + 
+  #theme(axis.title.y = element_blank()) +
+  #theme(axis.text.x = element_blank()) + 
+  theme(legend.position = "bottom")
+  #geom_boxplot()
+
+
+
+#header = c("rname","startpos","endpos","numreads","covbases","coverage","meandepth","meanbaseq","meanmapq")
+ggplot(df,aes(y=numreads,x=type,fill=type)) + 
+  geom_boxplot()
+ggplot(df,aes(y=covbases,x=type,fill=type)) + 
+  geom_boxplot()
+ggplot(df,aes(y=coverage,x=type,fill=type)) + 
+  geom_boxplot()
+ggplot(df,aes(y=meandepth,x=type,fill=type)) + 
+  geom_boxplot()
+ggplot(df,aes(y=meanbaseq,x=type,fill=type)) + 
+  geom_boxplot()
+ggplot(df,aes(y=meanmapq,x=type,fill=type)) + 
+  geom_boxplot()
+
+
+ggplot(df,aes(y=colnames(df)[1],x=type,fill=type)) + 
+  geom_boxplot()
+
+
+ggplot(df,aes(y=colnames(df)[1],x=type,fill=type)) + 
+  geom_boxplot()
+colnames(df)
+
+#[1] "rname"           "startpos"        "endpos"          "numreads"        "covbases"        "coverage"        "meandepth"      
+#[8] "meanbaseq"       "meanmapq"        "ID"              "type"            "mean_readLength"
 
 
 #rname	Reference name / chromosome
@@ -58,47 +109,8 @@ for (i in 4:9) {
 #meanbaseq	Mean baseQ in covered region
 #meanmapq	Mean mapQ of selected reads
 
-g1 <- ggplot(df,aes(y=numreads,x=type,fill=type)) + 
-  geom_boxplot() +
-  xlab("Number Reads aligned") + 
-  theme(axis.title.y = element_blank()) +
-  theme(axis.text.x = element_blank()) + 
-  theme(legend.position = "none")
 
-
-g2<-ggplot(df,aes(y=meandepth,x=type,fill=type)) + 
-  xlab("Mean depth of coverage") + 
-  theme(axis.title.y = element_blank()) +
-  theme(axis.text.x = element_blank()) + 
-  theme(legend.position = "none")+
-  geom_boxplot()
-
-g3<-ggplot(df,aes(y=meanmapq,x=type,fill=type)) + 
-  xlab("Mean mapQ of selected reads") + 
-  theme(axis.title.y = element_blank()) +
-  theme(axis.text.x = element_blank()) + 
-  theme(legend.position = "none")+
-  geom_boxplot()
-g4 <- cowplot::get_legend(ggplot(df,aes(y=meandepth,x=type,fill=type)) + 
-                            geom_boxplot())
-
-
-legend <- get_legend(ggplot(df,aes(y=meandepth,x=type,fill=type)) + 
-                       geom_boxplot() +  theme(legend.position = "top") + 
-                       theme(legend.key = element_rect(fill = "white")))
-
-
-grid.draw(g4)
-head(df)
-library(grid)
-library(gridExtra)
-grid.arrange(g1,g2, g3,g4,ncol = 4)
-grid.arrange(arrangeGrob(g1,g2, g3, ncol=3),g4,ncol = 2)
-grid.arrange(g1, g2, g3,legend, ncol=3, nrow = 2, 
-             layout_matrix = rbind(c(1,2,3), c(4,4,4)),
-             widths = c(2.7, 2.7, 2.7), heights = c(2, 0.3))
-
-
+dev.off()
 g1 <- ggplot(df,aes(y=numreads,x=type,fill=type)) + 
   geom_boxplot() +
   xlab("Number Reads aligned") + 
@@ -151,7 +163,7 @@ get_legend<-function(myggplot){
 }
 
 legend <- get_legend(ggplot(df,aes(y=meandepth,x=type,fill=type)) + 
-                       geom_boxplot() +  theme(legend.position = "top") + 
+                            geom_boxplot() +  theme(legend.position = "top") + 
                        theme(legend.title = element_blank()) + 
                        theme(legend.key = element_rect(fill = "white")))
 
@@ -165,13 +177,25 @@ library(ggplot2)
 
 
 grid.arrange(g1, g2, g3,g4,g5,g6,legend, ncol=3, nrow = 3, 
-             top = textGrob("Short-Read Bam statistics"),
+             top = textGrob("Long-Read Bam statistics"),
              layout_matrix = rbind(c(1,2,3), c(4,5 ,6), c(7,7,7)),
              widths = c(2.7, 2.7,2.7), heights = c(2.5, 2.5,0.3))
 
 
 
-#######
+
+
+grid.arrange(g1, g2, g3,g4, ncol=3, nrow = 2, 
+             layout_matrix = rbind(c(1,2,3), c(4,4,4)),
+             widths = c(2.7, 2.7, 2.7), heights = c(2.5, 0.2))
+layout_matrix
+
+grid.arrange(g1,g2, g3,g4,ncol = 3,nrow=2,layout_matrix = rbind(c(1,3), c(4,4)))
+
+layout_matrix = rbind(c(1,2), c(3,3))
+layout_matrix
+grid.arrange(arrangeGrob(g1,g2, g3, ncol=3),g4,ncol = 2)
+
 ggbarplot(gg_data, x = "count", y = "sample", fill = "len") +
   facet_grid(~type) + 
   theme(axis.text.x=element_text(size=7,angle=90), 
